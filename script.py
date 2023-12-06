95848e910168e06bba398fab834e2f25085811bb
Mehdi <sadour.mehdi@gmail.com>
2023-12-06 05:22:29 +0100

    add function calculate_distance_travelled.

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import random
 import pandas as pd
 
 from data.models import LocationClient, LocationEngineer, Engineer
-from utils import generate_visits_assigned_as_text
+from utils import convert_visit_as_text, calculate_total_km
 
 
 def distance_matrix() -> pd.DataFrame:
@@ -11,7 +11,7 @@ def distance_matrix() -> pd.DataFrame:
     locations_engineer = LocationEngineer.objects.all()
     locations_client = LocationClient.objects.all()
     for location_engineer in locations_engineer:
-        matrix[location_engineer.name] = [
+        matrix[location_engineer.engineer.full_name()] = [
             location_client.get_distance_another_location(location_engineer)
             for location_client in locations_client
         ]
@@ -20,7 +20,7 @@ def distance_matrix() -> pd.DataFrame:
     return df
 
 
-def random_installation_visits() -> str:
+def build_random_installation_visits() -> dict:
     engineers = Engineer.objects.all()
 
     locations_client_available = [lc for lc in LocationClient.objects.all()]
@@ -30,5 +30,27 @@ def random_installation_visits() -> str:
         engineer = engineers.order_by('?').first()
         location_client_available.engineer_visit = engineer
 
-    visits = generate_visits_assigned_as_text(locations_client_available)
+    visits = {}
+    for location in locations_client_available:
+        if location.engineer_visit.full_name() not in visits.keys():
+            visits[location.engineer_visit.full_name()] = []
+        visits[location.engineer_visit.full_name()].append(location.name)
+
     return visits
+
+
+def display_random_installation_visits() -> str:
+    locations_client_available = build_random_installation_visits()
+    visits = convert_visit_as_text(locations_client_available)
+    return visits
+
+
+def calculate_distance_travelled(engineer_name: str) -> int:
+    distances = distance_matrix()
+    visits = build_random_installation_visits()
+    visits_of_engineer = visits.get(engineer_name)
+    df = pd.DataFrame(distances, columns=[engineer_name])
+    df = df.filter(items=visits_of_engineer, axis=0)
+    distances_in_km = df[engineer_name].values.tolist()
+    total_km = calculate_total_km(distances_in_km)
+    return total_km
--- utils.py
+++ utils.py
@@ -1,16 +1,19 @@
-from data.models import LocationClient
-
-
-def generate_visits_assigned_as_text(locations_client: list[LocationClient]):
-    visits = {}
-    for location in locations_client:
-        if location.engineer_visit.full_name() not in visits.keys():
-            visits[location.engineer_visit.full_name()] = []
-        visits[location.engineer_visit.full_name()].append(location.name)
-
+def convert_visit_as_text(visits: dict) -> str:
     visits_as_list = [
         f"{engineer} visit {locations}"
         for engineer, locations in visits.items()
     ]
     visits_as_text = ". ".join(visits_as_list)
     return visits_as_text
+
+
+def calculate_total_km(distances_km: list) -> int:
+    total = 0
+    for index, distance in enumerate(distances_km):
+        if index == 0:
+            total += distance
+        elif index > 0:
+            difference = abs(distance - distances_km[index -1])
+            total += difference
+
+    return total
