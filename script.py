0e61e27a653ac52187a33d1f301bd9e851f52349
Mehdi <sadour.mehdi@gmail.com>
2023-12-06 02:51:03 +0100

    Use dataframe for matrix.

--- script.py
+++ script.py
@@ -1,13 +1,17 @@
+import pandas as pd
+
 from data.models import LocationClient, LocationEngineer
 
 
-def distance_matrix() -> dict:
+def distance_matrix() -> pd.DataFrame:
     matrix: dict = {}
     locations_engineer = LocationEngineer.objects.all()
     locations_client = LocationClient.objects.all()
     for location_engineer in locations_engineer:
         matrix[location_engineer.name] = [
-            {location_client.name: location_client.display_distance_with_name(location_engineer)}
+            f"{location_client.name} is {location_client.display_distance_with_name(location_engineer)}"
             for location_client in locations_client
         ]
-    return matrix
+
+    df = pd.DataFrame(data=matrix)
+    return df
